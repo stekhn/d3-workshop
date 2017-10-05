501e5633069f152d7adff97774fddcf374b34700
Steffen Kühne <stekhn@gmail.com>
2017-10-05 21:42:31 +0200

    Add dependencies and dotfiles

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+{
+  "globals": {
+    "d3": true,
+    "L": true
+  },
+  "rules": {
+    "indent": [2, 2, {"SwitchCase": 1}],
+    "quotes": [2, "single"],
+    "linebreak-style": [2, "unix"],
+    "semi": [2, "always"],
+    "comma-dangle": [1, "never"],
+    "no-trailing-spaces": [2, {"skipBlankLines": true}]
+  },
+  "env": {
+    "node": true,
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended"
+}
